524bc76f1598b7924be13fd77cf9aa4f39bf6b40
James Duncombe <james.duncombe@consilienceventures.com>
2022-06-24 16:54:30 +0100

    Adds tests for FastTopFacet.

--- test/fast/FastTopFacet.test.ts
+++ test/fast/FastTopFacet.test.ts
@@ -5,9 +5,9 @@ import { BigNumber } from 'ethers';
 import { deployments, ethers } from 'hardhat';
 import { FakeContract, smock } from '@defi-wonderland/smock';
 import { SignerWithAddress } from 'hardhat-deploy-ethers/signers';
-import { negOne, one, oneMillion, REQUIRES_FAST_GOVERNORSHIP, REQUIRES_SPC_MEMBERSHIP } from '../utils';
+import { negOneHundred, oneHundred, ninety, REQUIRES_SPC_MEMBERSHIP, MISSING_ATTACHED_ETH, INTERNAL_METHOD } from '../utils';
 import { DEPLOYER_FACTORY_COMMON, toHexString } from '../../src/utils';
-import { Spc, Exchange, Fast, FastTopFacet, FastInitFacet, FastTokenFacet, FastAccessFacet } from '../../typechain';
+import { Spc, Exchange, Fast, FastTopFacet } from '../../typechain';
 chai.use(solidity);
 chai.use(smock.matchers);
 
@@ -44,25 +44,21 @@ const fastDeployFixture = deployments.createFixture(async (hre, uOpts) => {
   });
 });
 
-describe('FastAccessFacet', () => {
+describe('FastTopFacet', () => {
   let
     deployer: SignerWithAddress,
     spcMember: SignerWithAddress,
     governor: SignerWithAddress,
-    alice: SignerWithAddress,
-    bob: SignerWithAddress,
-    rob: SignerWithAddress,
-    john: SignerWithAddress;
+    bob: SignerWithAddress;
   let spc: FakeContract<Spc>,
     exchange: FakeContract<Exchange>,
-    access: FastAccessFacet,
-    governedAccess: FastAccessFacet,
-    spcMemberAccess: FastAccessFacet;
-
+    token: FastTopFacet,
+    spcMemberToken: FastTopFacet,
+    deployerToken: FastTopFacet;
 
   before(async () => {
     // Keep track of a few signers.
-    [deployer, spcMember, governor, alice, bob, rob, john] = await ethers.getSigners();
+    [deployer, spcMember, governor, bob] = await ethers.getSigners();
     // Mock an SPC and an Exchange contract.
     spc = await smock.fake('Spc');
     exchange = await smock.fake('Exchange');
@@ -89,20 +85,76 @@ describe('FastAccessFacet', () => {
 
     // TODO: Once smock fixes their stuff. replace facets by fakes.
 
-    access = fast as FastAccessFacet;
-    governedAccess = access.connect(governor);
-    spcMemberAccess = access.connect(spcMember);
+    token = fast as FastTopFacet;
+    spcMemberToken = token.connect(spcMember);
+    deployerToken = token.connect(deployer);
   });
 
+  // Getters.
+
+  describe('spcAddress', async () => {
+    it('returns the SPC address', async () => {
+      const subject = await spcMemberToken.spcAddress();
+      expect(subject).to.eq(spc.address);
+    });
+  });
+
+  describe('exchangeAddress', async () => {
+    it('returns the exchange address', async () => {
+      const subject = await spcMemberToken.exchangeAddress();
+      expect(subject).to.eq(exchange.address);
+    });
+  });
+
+  // Provisioning functions.
+
   describe('provisionWithEth', async () => {
-    it('NEEDS MORE TESTS');
+    it('reverts when no Eth is attached', async () => {
+      const subject = spcMemberToken.provisionWithEth();
+      await expect(subject).to.be
+        .revertedWith(MISSING_ATTACHED_ETH);
+    });
+
+    it('emits a EthReceived event', async () => {
+      const subject = spcMemberToken.provisionWithEth({ value: ninety });
+      await expect(subject).to
+        .emit(token, 'EthReceived')
+        .withArgs(spcMember.address, ninety)
+    });
   });
 
   describe('drainEth', async () => {
-    it('NEEDS MORE TESTS');
+    it('requires SPC membership', async () => {
+      const subject = token.drainEth();
+      await expect(subject).to.be
+        .revertedWith(REQUIRES_SPC_MEMBERSHIP);
+    });
+
+    it('transfers all the locked Eth to the caller', async () => {
+      // Provision the token with a lot of Eth.
+      await ethers.provider.send("hardhat_setBalance", [token.address, toHexString(oneHundred)]);
+      // Drain the token.
+      const subject = async () => await spcMemberToken.drainEth();
+      await expect(subject).to.changeEtherBalances([token, spcMember], [negOneHundred, oneHundred]);
+    });
+
+    it('emits a EthDrained event', async () => {
+      // Provision the token with a lot of Eth.
+      await ethers.provider.send("hardhat_setBalance", [token.address, toHexString(oneHundred)]);
+      // Drain the token.
+      const subject = spcMemberToken.drainEth();
+      await expect(subject).to
+        .emit(token, 'EthDrained')
+        .withArgs(spcMember.address, oneHundred);
+    });
   });
 
   describe('payUpTo', async () => {
-    it('NEEDS MORE TESTS');
+    it('cannot be called directly', async () => {
+      await expect(spcMemberToken.payUpTo(bob.address, ninety)).to.be
+        .revertedWith(INTERNAL_METHOD);
+    });
+
+    it('requires the recipient to be non-zero address');
   });
 });
