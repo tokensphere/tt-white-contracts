3bf1e8ff59c0a5b3ccc3fa9cfb8289fd540f0d3c
Pierre Martin <hickscorp@gmail.com>
2022-07-13 16:38:01 +0200

    Fixes failing test.

--- test/spc/SpcFrontendFacet.test.ts
+++ test/spc/SpcFrontendFacet.test.ts
@@ -3,26 +3,42 @@ import { expect } from 'chai';
 import { solidity } from 'ethereum-waffle';
 import { deployments, ethers } from 'hardhat';
 import { FakeContract, smock } from '@defi-wonderland/smock';
-import { Spc, Exchange, Fast, SpcFrontendFacet } from '../../typechain';
+import { Spc, Exchange, Fast, SpcFrontendFacet, FastFrontendFacet } from '../../typechain';
 import { SignerWithAddress } from 'hardhat-deploy-ethers/signers';
 import { BigNumber } from 'ethers';
 import { FAST_INIT_DEFAULTS } from '../fixtures/fast';
 import { spcFixtureFunc } from '../fixtures/spc';
+import { ZERO_ADDRESS } from '../../src/utils';
 chai.use(solidity);
 chai.use(smock.matchers);
 
+const FAST_DETAILS_DEFAULTS: FastFrontendFacet.DetailsStruct = {
+  name: FAST_INIT_DEFAULTS.name,
+  symbol: FAST_INIT_DEFAULTS.symbol,
+  decimals: FAST_INIT_DEFAULTS.decimals,
+  hasFixedSupply: FAST_INIT_DEFAULTS.hasFixedSupply,
+  isSemiPublic: FAST_INIT_DEFAULTS.isSemiPublic,
+  addr: ZERO_ADDRESS,
+  totalSupply: BigNumber.from(20),
+  transferCredits: BigNumber.from(30),
+  reserveBalance: BigNumber.from(40),
+  ethBalance: BigNumber.from(0),
+  memberCount: BigNumber.from(1),
+  governorCount: BigNumber.from(2)
+};
+
 describe('SpcFrontendFacet', () => {
   let
-    deployer: SignerWithAddress,
-    spcMember: SignerWithAddress,
-    governor: SignerWithAddress;
+    deployer: Readonly<SignerWithAddress>,
+    spcMember: Readonly<SignerWithAddress>,
+    governor: Readonly<SignerWithAddress>;
   let
-    exchange: FakeContract<Exchange>,
-    fast: FakeContract<Fast>;
+    exchange: Readonly<FakeContract<Exchange>>,
+    fasts: ReadonlyArray<FakeContract<Fast>>;
   let
-    spc: Spc,
-    spcMemberSpc: Spc,
-    frontend: SpcFrontendFacet;
+    spc: Readonly<Spc>,
+    spcMemberSpc: Readonly<Spc>,
+    frontend: Readonly<SpcFrontendFacet>;
 
   const spcDeployFixture = deployments.createFixture(spcFixtureFunc);
 
@@ -43,21 +59,19 @@ describe('SpcFrontendFacet', () => {
           // Mock the Exchange.
           exchange = await smock.fake('Exchange');
           exchange.spcAddress.returns(res.spc.address);
+
           // Prepare a fake FAST.
-          fast = await smock.fake<Fast>('Fast');
-          fast.symbol.returns(FAST_INIT_DEFAULTS.symbol);
-          fast.details.reset();
-          fast.details.returns({
-            ...FAST_INIT_DEFAULTS,
-            addr: fast.address,
-            totalSupply: BigNumber.from(20),
-            transferCredits: BigNumber.from(30),
-            reserveBalance: BigNumber.from(40),
-            memberCount: BigNumber.from(1),
-            governorCount: BigNumber.from(2),
-          });
-          // Register the FAST.
-          await spcMemberSpc.registerFast(fast.address);
+          const values: Array<FakeContract<Fast>> = [];
+          for (const index of [0, 1, 2]) {
+            const fast = await smock.fake<Fast>('Fast');
+            const symbol = `F0${index}`;
+            fast.symbol.returns(symbol);
+            fast.details.returns({ ...FAST_DETAILS_DEFAULTS, symbol });
+            // Register the FAST.
+            await spcMemberSpc.registerFast(fast.address);
+            values.push(fast);
+          }
+          fasts = [...values];
         }
       },
       initWith: {
@@ -68,11 +82,10 @@ describe('SpcFrontendFacet', () => {
 
   describe('paginateDetailedFasts', async () => {
     it('returns a paginated list of detailed FAST details', async () => {
-      // TODO: Why isn't this deserializing properly?...
-      // const [[{ name }], nextCursor] = await spcMemberSpc.paginateDetailedFasts(0, 5);
-      // expect(fast.details).to.have.been.calledOnceWith();
-      // expect(name).to.eq('Fast 1');
-      // expect(nextCursor).to.eq(2);
+      const [/*results*/, nextCursor] = await spcMemberSpc.paginateDetailedFasts(0, 5);
+      for (const fast of fasts)
+        expect(fast.details).to.have.been.calledOnceWith();
+      expect(nextCursor).to.eq(3);
     });
   });
 });
