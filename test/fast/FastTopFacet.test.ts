e8a37cd7b78ceefc8a9c459721476cabaa07da2f
Pierre Martin <hickscorp@gmail.com>
2022-07-14 14:01:33 +0200

    Adds missing FastTopFacet test placeholders, improve tests, improves style.

--- test/fast/FastTopFacet.test.ts
+++ test/fast/FastTopFacet.test.ts
@@ -17,7 +17,7 @@ import {
 } from '../utils';
 import { toUnpaddedHexString, ZERO_ADDRESS } from '../../src/utils';
 import { Spc, Exchange, FastTopFacet, Fast } from '../../typechain';
-import { fastFixtureFunc } from '../fixtures/fast';
+import { fastFixtureFunc, FAST_INIT_DEFAULTS } from '../fixtures/fast';
 chai.use(solidity);
 chai.use(smock.matchers);
 
@@ -31,8 +31,9 @@ describe('FastTopFacet', () => {
   let spc: FakeContract<Spc>,
     exchange: FakeContract<Exchange>,
     fast: Fast,
-    token: FastTopFacet,
-    spcMemberToken: FastTopFacet;
+    top: FastTopFacet,
+    spcMemberTop: FastTopFacet,
+    topAsItself: FastTopFacet;
 
   const fastDeployFixture = deployments.createFixture(fastFixtureFunc);
 
@@ -52,8 +53,8 @@ describe('FastTopFacet', () => {
         deployer: deployer.address,
         afterDeploy: async (args) => {
           ({ fast } = args);
-          token = await ethers.getContractAt<FastTopFacet>('FastTopFacet', fast.address);
-          spcMemberToken = token.connect(spcMember);
+          top = await ethers.getContractAt<FastTopFacet>('FastTopFacet', fast.address);
+          spcMemberTop = top.connect(spcMember);
         }
       },
       initWith: {
@@ -63,32 +64,45 @@ describe('FastTopFacet', () => {
       }
     });
 
+    topAsItself = await impersonateContract(top)
+
     // Set the SPC member.
     spc.isMember.reset();
     spc.isMember.whenCalledWith(spcMember.address).returns(true);
     spc.isMember.returns(false);
   });
 
+  afterEach(async () => {
+    await ethers.provider.send("hardhat_stopImpersonatingAccount", [top.address]);
+  });
+
   // Getters.
 
   describe('spcAddress', async () => {
     it('returns the SPC address', async () => {
-      const subject = await spcMemberToken.spcAddress();
+      const subject = await spcMemberTop.spcAddress();
       expect(subject).to.eq(spc.address);
     });
   });
 
   describe('exchangeAddress', async () => {
     it('returns the exchange address', async () => {
-      const subject = await spcMemberToken.exchangeAddress();
+      const subject = await spcMemberTop.exchangeAddress();
       expect(subject).to.eq(exchange.address);
     });
   });
 
+  describe('isSemiPublic', async () => {
+    it('returns the FAST semi-public parameter', async () => {
+      const subject = await top.isSemiPublic();
+      expect(subject).to.eq(FAST_INIT_DEFAULTS.isSemiPublic);
+    });
+  });
+
   describe('hasFixedSupply', async () => {
-    it('returns the token fixed supply parameter', async () => {
-      const subject = await token.hasFixedSupply();
-      expect(subject).to.eq(true);
+    it('returns the FAST fixed supply parameter', async () => {
+      const subject = await top.hasFixedSupply();
+      expect(subject).to.eq(FAST_INIT_DEFAULTS.hasFixedSupply);
     });
   });
 
@@ -96,94 +110,93 @@ describe('FastTopFacet', () => {
 
   describe('setIsSemiPublic', async () => {
     it('requires SPC membership for the sender', async () => {
-      const subject = token.setIsSemiPublic(true);
+      const subject = top.setIsSemiPublic(true);
       await expect(subject).to.be
         .revertedWith(REQUIRES_SPC_MEMBERSHIP);
     });
 
+    it('delegates to the SPC for permission check');
+
     it('cannot revert an SPC to non-semi public once set', async () => {
       // Set as semi public.
-      await spcMemberToken.setIsSemiPublic(true);
+      await spcMemberTop.setIsSemiPublic(true);
       // Attempt to revert to non-semi public.
-      const subject = spcMemberToken.setIsSemiPublic(false);
+      const subject = spcMemberTop.setIsSemiPublic(false);
       await expect(subject).to.be
         .revertedWith(UNSUPPORTED_OPERATION);
     });
 
-    it('sets the required isSemiPublic flag on the token', async () => {
-      await spcMemberToken.setIsSemiPublic(true);
-      expect(await spcMemberToken.isSemiPublic()).to.be.true;
+    it('sets the required flag on the FAST', async () => {
+      await spcMemberTop.setIsSemiPublic(true);
+      expect(await spcMemberTop.isSemiPublic()).to.be.true;
     });
+
+    it('delegates to FastFrontendFacet.emitDetailsChanged');
   });
 
   // Provisioning functions.
 
   describe('provisionWithEth', async () => {
     it('reverts when no Eth is attached', async () => {
-      const subject = spcMemberToken.provisionWithEth();
+      const subject = spcMemberTop.provisionWithEth();
       await expect(subject).to.be
         .revertedWith(MISSING_ATTACHED_ETH);
     });
 
     it('emits a EthReceived event', async () => {
-      const subject = await spcMemberToken.provisionWithEth({ value: ninety });
+      const subject = await spcMemberTop.provisionWithEth({ value: ninety });
       await expect(subject).to
         .emit(fast, 'EthReceived')
         .withArgs(spcMember.address, ninety)
     });
+
+    it('delegates to FastFrontendFacet.emitDetailsChanged');
   });
 
   describe('drainEth', async () => {
     it('requires SPC membership', async () => {
-      const subject = token.drainEth();
+      const subject = top.drainEth();
       await expect(subject).to.be
         .revertedWith(REQUIRES_SPC_MEMBERSHIP);
     });
+
+    it('delegates to the SPC for permission check');
 
     it('requires that the caller is not a contract');
 
     it('transfers all the locked Eth to the caller', async () => {
       // Provision the FAST with a lot of Eth.
-      await ethers.provider.send("hardhat_setBalance", [token.address, toUnpaddedHexString(oneHundred)]);
+      await ethers.provider.send("hardhat_setBalance", [top.address, toUnpaddedHexString(oneHundred)]);
       // Drain the FAST.
-      const subject = async () => await spcMemberToken.drainEth();
-      await expect(subject).to.changeEtherBalances([token, spcMember], [negOneHundred, oneHundred]);
+      const subject = async () => await spcMemberTop.drainEth();
+      await expect(subject).to.changeEtherBalances([top, spcMember], [negOneHundred, oneHundred]);
     });
 
     it('emits a EthDrained event', async () => {
       // Provision the FAST with a lot of Eth.
-      await ethers.provider.send("hardhat_setBalance", [token.address, toUnpaddedHexString(oneHundred)]);
+      await ethers.provider.send("hardhat_setBalance", [top.address, toUnpaddedHexString(oneHundred)]);
       // Drain the FAST.
-      const subject = await spcMemberToken.drainEth();
+      const subject = await spcMemberTop.drainEth();
       await expect(subject).to
         .emit(fast, 'EthDrained')
         .withArgs(spcMember.address, oneHundred);
     });
+
+    it('delegates to FastFrontendFacet.emitDetailsChanged');
   });
 
   describe('payUpTo', async () => {
     it('cannot be called directly', async () => {
-      await expect(spcMemberToken.payUpTo(bob.address, ninety)).to.be
+      await expect(spcMemberTop.payUpTo(bob.address, ninety)).to.be
         .revertedWith(INTERNAL_METHOD);
     });
 
-    describe('as diamondInternal', async () => {
-      let tokenAsItself: FastTopFacet;
+    it('requires that the caller is not a contract');
 
-      beforeEach(async () => {
-        // Impersonate the diamond.
-        tokenAsItself = await impersonateContract(token);
-      });
-
-      afterEach(async () => {
-        await ethers.provider.send("hardhat_stopImpersonatingAccount", [token.address]);
-      });
-
-      it('requires the recipient to be non-zero address', async () => {
-        const subject = tokenAsItself.payUpTo(ZERO_ADDRESS, ninety);
-        await expect(subject).to.be
-          .revertedWith(REQUIRES_NON_ZERO_ADDRESS);
-      });
+    it('requires the recipient to be non-zero address', async () => {
+      const subject = topAsItself.payUpTo(ZERO_ADDRESS, ninety);
+      await expect(subject).to.be
+        .revertedWith(REQUIRES_NON_ZERO_ADDRESS);
     });
   });
 });
