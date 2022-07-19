1504e787ca0992b41c7c8bfe0f610be986a56897
James Duncombe <james.duncombe@consilienceventures.com>
2022-07-19 14:33:51 +0100

    Renames interface parameter names.

--- contracts/interfaces/IHasMembers.sol
+++ contracts/interfaces/IHasMembers.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.4;
 
 
 interface IHasMembers {
-  function isMember(address a) external view returns(bool);
+  function isMember(address member) external view returns(bool);
   function memberCount() external view returns(uint256);
   function paginateMembers(uint256 index, uint256 perPage) external view returns(address[] memory, uint256);
-  function addMember(address payable a) external;
-  function removeMember(address a) external;
+  function addMember(address payable member) external;
+  function removeMember(address member) external;
 }
