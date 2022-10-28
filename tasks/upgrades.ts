d2093435b17ac2ef6137a78edaa37799deff6400
Pierre Martin <hickscorp@gmail.com>
2022-10-28 13:18:16 +0100

    Adds a task allowing to update all deployed diamonds.

---
+++ tasks/upgrades.ts
@@ -0,0 +1,21 @@
+import { task } from 'hardhat/config';
+import fs from 'fs';
+
+// Tasks.
+
+task('update-facets', 'Updates facets of all deployed contracts')
+  .setAction(async (params: {}, hre) => {
+    const { run } = hre;
+    // Update issuer and marketplace facets.
+    await run('issuer-update-facets');
+    await run('marketplace-update-facets');
+
+    // Iterate over all deployment artifacts for the current network.
+    const fastFilenames = fs
+      .readdirSync(`deployments/${hre.network.name}`)
+      .filter(fn => fn.startsWith('Fast') && fn.endsWith('_DiamondProxy.json'))
+    for (const fn of fastFilenames) {
+      const symbol = fn.slice(4, -17);
+      await run(`fast-update-facets ${symbol}`);
+    }
+  });
