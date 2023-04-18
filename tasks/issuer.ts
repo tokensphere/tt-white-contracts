a6f87db8e0a67d82fd0507b68c8d4e1af08ac911
Pierre Martin <hickscorp@gmail.com>
2023-04-18 16:50:51 +0100

    Midway through polygon deployment...

--- tasks/issuer.ts
+++ tasks/issuer.ts
@@ -30,14 +30,22 @@ task("issuer-update-facets", "Updates facets of our Issuer").setAction(
       deterministicSalt: deploymentSalt(hre),
       log: true,
     });
-  },
+  }
 );
 
 // Reusable functions.
 
-const ISSUER_FACETS = [...COMMON_DIAMOND_FACETS, "IssuerTopFacet", "IssuerAccessFacet", "IssuerFrontendFacet"];
+const ISSUER_FACETS = [
+  ...COMMON_DIAMOND_FACETS,
+  "IssuerTopFacet",
+  "IssuerAccessFacet",
+  "IssuerFrontendFacet",
+];
 
-const deployIssuer = async (hre: HardhatRuntimeEnvironment, issuerMember: string): Promise<Issuer> => {
+const deployIssuer = async (
+  hre: HardhatRuntimeEnvironment,
+  issuerMember: string
+): Promise<Issuer> => {
   const {
     ethers,
     deployments,
@@ -48,7 +56,9 @@ const deployIssuer = async (hre: HardhatRuntimeEnvironment, issuerMember: string
 
   let deploy = await deployments.getOrNull("Issuer");
   if (deploy) {
-    console.log(`Issuer already deployed at ${deploy.address}, skipping deployment.`);
+    console.log(
+      `Issuer already deployed at ${deploy.address}, skipping deployment.`
+    );
   } else {
     console.log("Deploying Issuer...");
     // Deploy the diamond with an additional initialization facet.
@@ -69,7 +79,10 @@ const deployIssuer = async (hre: HardhatRuntimeEnvironment, issuerMember: string
     // Initialize the diamond. We are doing it in two steps, because the Issuer member is different
     // in each environment, and this would make our deployment transaction different in each and
     // therefore defeat the deterministic deployment strategy.
-    const issuerInitFacet = await ethers.getContractAt("IssuerInitFacet", deploy.address);
+    const issuerInitFacet = await ethers.getContractAt(
+      "IssuerInitFacet",
+      deploy.address
+    );
     await (await issuerInitFacet.initialize({ member: issuerMember })).wait();
   }
 
