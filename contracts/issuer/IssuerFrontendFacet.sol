c6e2c0068276b21cddf0d190af25c504b4d46c23
Pierre Martin <hickscorp@gmail.com>
2023-05-22 16:13:32 +0100

    Adds an issuer frontend detailed pagination method for groups.

--- contracts/issuer/IssuerFrontendFacet.sol
+++ contracts/issuer/IssuerFrontendFacet.sol
@@ -31,4 +31,26 @@ contract IssuerFrontendFacet is AIssuerFacet {
     }
     return (fastDetails, nextCursor);
   }
+
+  /**
+   * @notice Paginates the FAST diamonds registered with this Issuer based on a group, starting cursor and a
+   *        number of records per page. It returns rich details for each FAST diamond.
+   * @param group The group to paginate.
+   * @param cursor The index at which to start.
+   * @param perPage How many records should be returned at most.
+   * @return A `address[]` list of values at most `perPage` big.
+   * @return A `uint256` index to the next page.
+   */
+  function paginateDetailedFastsInGroup(string calldata group, uint256 cursor, uint256 perPage)
+      external view
+      returns(FastFrontendFacet.Details[] memory, uint256) {
+    (address[] memory addresses, uint256 nextCursor) = LibPaginate.addresses(LibIssuer.data().fastGroups[group].values, cursor, perPage);
+    FastFrontendFacet.Details[] memory fastDetails = new FastFrontendFacet.Details[](addresses.length);
+    uint256 length = addresses.length;
+    for (uint256 i = 0; i < length;) {
+      fastDetails[i] = FastFrontendFacet(addresses[i]).details();
+      unchecked { ++i; }
+    }
+    return (fastDetails, nextCursor);
+  }
 }
