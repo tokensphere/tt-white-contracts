d79cd1887bde235c963cd467cff2dc8d3064af67
Pierre Martin <hickscorp@gmail.com>
2023-06-06 13:04:38 +0200

    Adds privileges for crowdfund management.

--- contracts/issuer/IssuerAutomatonsFacet.sol
+++ contracts/issuer/IssuerAutomatonsFacet.sol
@@ -5,9 +5,7 @@ import './lib/AIssuerFacet.sol';
 import '../common/AHasAutomatons.sol';
 
 // Privileges bits.
-// TODO: Propagate trait to various methods.
-uint32 constant ISSUER_PRIVILEGE_GOVERNANCE = 1;
-uint32 constant ISSUER_PRIVILEGE_CROWDFUND_CREATOR = 2;
+uint32 constant ISSUER_PRIVILEGE_CROWDFUND_CREATOR = 1;
 
 /**
  * @title The Issuer Smart Contract.
